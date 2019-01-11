6bfeaa69eaa80eb681eb81fca5830b061e6ef57b
Luca Colagiorgio <l.colagiorgio@hotmail.com>
2019-01-11 14:35:40 +0100

    design login
    design login

--- dist/register.php
+++ dist/register.php
@@ -80,10 +80,11 @@
 
   <!-- Content begin -->
   <br>
+  <div class="signin-form">
   <div class="container">
-      <div class="signin-form">
-        <form>
-          <div class="col col-md-offset-1">
+    <form>
+      <div class="col-md-15 col-md-offset-5">
+        <div class="form-row">
               <div class="form-group col-md-6">
                   <label for="inputEmail4">Email</label>
                   <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
@@ -92,10 +93,41 @@
                   <label for="inputPassword4">Password</label>
                   <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
               </div>
-              <button type="submit" class="btn btn-outline-success my-2 my-sm-0">Sign in</button>
           </div>
-        </form>
+          <div class="form-group">
+              <label for="inputAddress">Address</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+          </div>
+          <div class="form-group">
+              <label for="inputAddress2">Address 2</label>
+              <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+          </div>
+          <div class="form-row">
+              <div class="form-group col-md-6">
+                  <label for="inputCity">City</label>
+                  <input type="text" class="form-control" id="inputCity">
+              </div>
+              <div class="form-group col-md-4">
+                  <label for="inputState">State</label>
+                  <input type="text" class="form-control" id="inputCity">
+              </div>
+              <div class="form-group col-md-2">
+                  <label for="inputZip">Zip</label>
+                  <input type="text" class="form-control" id="inputZip">
+              </div>
+          </div>
+          <div class="form-group">
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="gridCheck">
+                  <label class="form-check-label" for="gridCheck">
+                      Check me out
+                  </label>
+              </div>
+          </div>
+          <button type="submit" class="btn btn-primary">Sign in</button>
       </div>
+    </form>
+  </div>
   </div>
 
   <script type="text/javascript" src="assets/js/bootstrap.js"></script>
